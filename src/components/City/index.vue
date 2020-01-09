1b482da8a2c7c02d578f58522e871474b9953d06
wukaige <1641656670@qq.com>
2020-01-09 08:22:38 +0800

    添加城市组件

---
+++ src/components/City/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="city_body">
+    <div class="city_list">
+      <div class="city_hot">
+        <h2>热门城市</h2>
+        <ul>
+          <li v-for="item in HotCity" :key="item.id">{{item.nm}}</li>
+          <li>北京</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // HotCity: []
+      prop: {
+        city: HotCity
+      }
+    };
+  },
+  mounted() {
+    this.axios.get("/api/cityList").then(res => {
+      const cities = res.data.data.cities;
+      // console.log(cities);
+      let city = cities.filter(item => item.isHot == 1);
+      this.HotCity.push(city);
+      console.log(this.HotCity);
+    });
+  }
+};
+</script>
+
+<style scoped>
+#content .city_body {
+  margin-top: 45px;
+  display: flex;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+}
+.city_body .city_list {
+  flex: 1;
+  overflow: auto;
+  background: #fff;
+  margin-bottom: 15px;
+}
+.city_body .city_list::-webkit-scrollbar {
+  background-color: transparent;
+  width: 0;
+}
+.city_body .city_hot {
+  margin-top: 20px;
+}
+.city_body .city_hot h2 {
+  padding-left: 15px;
+  line-height: 30px;
+  font-size: 14px;
+  background: #f0f0f0;
+  font-weight: normal;
+}
+
+.city_body .city_hot ul li {
+  float: left;
+  background: #fff;
+  width: 29%;
+  height: 33px;
+  margin-top: 15px;
+  margin-left: 3%;
+  padding: 0 4px;
+  border: 1px solid #e6e6e6;
+  border-radius: 3px;
+  line-height: 33px;
+  text-align: center;
+  box-sizing: border-box;
+}
+</style>
